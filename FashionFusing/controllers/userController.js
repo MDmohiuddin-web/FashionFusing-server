fd133aa8ef97a654bccf95c607f49694cbb7df61
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-09-25 17:38:55 +0600

    ll

--- FashionFusing/controllers/userController.js
+++ FashionFusing/controllers/userController.js
@@ -1,11 +1,13 @@
 
 import validator from "validator";
-import userModel from "../models/UserModel";
+
+
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import userModel from "../models/UserModels.js"; 
 
 const createToken=(id)=>{
-  return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'3d'})
+  return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'6h'})
 
 }
 
@@ -18,20 +20,18 @@ const loginUser = async (req, res) => {
 
 //   route for user registration
 const registerUser = async (req, res) => {
-  // res.json({ message: "User registered successfully" });
-
   try {
     const { name, email, password } = req.body;
-    // checking for existing user
+    // checking for existing user 
     const existingUser = await userModel.findOne({email})
     if (existingUser) {
-      return res.json({success:false, message: "User already exists" });
+      return res.status(400).json({success:false, message: "User already exists" });
     }
     if (!validator.isEmail(email)) {
-      return res.json({success:false, message: "Invalid email address" });
+      return res.status(400).json({success:false, message: "Invalid email address" });
     }
     if(password.length < 8){
-      return res.json({success:false, message: "Password must be at least 8 characters " });
+      return res.status(400).json({success:false, message: "Password must be at least 8 characters " });
     }
     // hashing password
     const salt = await bcrypt.genSalt(10);
@@ -43,16 +43,11 @@ const registerUser = async (req, res) => {
     });
     // save user to database
     const user = await newUser.save();
-    res.json({success:true, message: "User registered successfully" });
-    console.log(user);
     const token = createToken(user._id);
-    res.json({success:true, token});
-
-
-    
+    res.status(201).json({success:true, message: "User registered successfully", token});
   } catch (error) { 
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({success:false, message: "Internal server error" });
   }
 };
 
--- FashionFusing/server.js
+++ FashionFusing/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
-import { connect } from 'mongoose';
+
 import clouding from './config/clouding.js';
 import userRouter from './routes/UserRouts.js';
 
